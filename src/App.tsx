4c84308858f68de526ae5807648781c461eb38ff
assarasua <23295000+assarasua@users.noreply.github.com>
2026-02-19 10:16:54 +0100

    Integrate InfiniteWatch provider and env configuration

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { InfiniteWatchProvider } from "@infinitewatch/react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useGeolocation } from "@/hooks/useGeolocation";
 import Index from "./pages/Index";
@@ -27,15 +28,17 @@ function AppContent() {
 }
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <AppContent />
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+  <InfiniteWatchProvider organizationId={import.meta.env.VITE_INFINITEWATCH_ORG_ID}>
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <AppContent />
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  </InfiniteWatchProvider>
 );
 
 export default App;
